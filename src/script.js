3991c6c01bf73fdd826da6da7af4ae77096a76c3
adarshcs99 <adarshsinghchauhan@cedcoss.com>
2022-02-14 13:57:06 +0530

    updated

--- src/script.js
+++ src/script.js
@@ -1,254 +1,184 @@
-$(document).ready(function(){
+// var table_add ="";
+var mall = [];
 
-    var products = [{
-        "id": "100",
-        "name": "iPhone 4S",
-        "brand": "Apple",
-        "os": "iOS"
-        
-    },
-    {
-        "id": "101",
-        "name": "Moto X",
-        "brand": "Motorola",
-        "os": "Android"	
-        
-    },
-    {
-        "id": "102",
-        "name": "iPhone 6",
-        "brand": "Apple",
-        "os": "iOS"
-        
-    },
-    {
-        "id": "103",
-        "name": "Samsung Galaxy S",
-        "brand": "Samsung",
-        "os": "Android"
-        
-    },
-    {
-        "id": "104",
-        "name": "Google Nexus",
-        "brand": "ASUS",
-        "os": "Android"
-        
-    },
-    {
-        "id": "105",
-        "name": "Surface",
-        "brand": "Microsoft",
-        "os": "Windows" 
-        
-    }];
-
-
-    var result="";
-    var brand="All";
-    var os="All";
-    var searchInput="";
-
-    result+=`<select name="brand" id="brand">
-        <option value="All">All</option>
-        <option value="Apple">Apple</option>
-        <option value="Motorola">Motorola</option>
-        <option value="Samsung">Samsung</option>
-        <option value="ASUS">ASUS</option>
-        <option value="Microsoft">Microsoft</option>
-      </select>
-      <select name="os" id="os">
-        <option value="All">All</option>
-        <option value="iOS">iOS</option>
-        <option value="Android">Android</option>
-        <option value="Windows">Windows</option>
-      </select><br>
-      <div id="disp"></div><br>
-      <div id="search"><label for="search"><input id="input" placeholder="search by name or id"><input type="button" id="button" value="search"></label></div>`; 
-
-      $('#content').html(result);
-
-
-      function displayAll()
-      {
-          var result1="";
-          if(searchInput=="")
-          {
-            for(let i=0;i<products.length;i++)
-            {
-                
-              result1+=`<table><tr><td style="padding-right:60px">${products[i].id}</td><td style="padding-right:60px">${products[i].name}</td><td style="padding-right:60px">${products[i].brand}</td><td style="padding-right:60px">${products[i].os}</td><td><a href="#" id="x" data-pid=${products[i].id}>X</td></tr></table>`;
-              //products[i].x=0;
-            }
-          }
-          else
-          {
-            for(let i=0;i<products.length;i++)
-            {
-                if(searchInput==products[i].id || searchInput==products[i].name)
-                result1+=`<table><tr><td style="padding-right:60px">${products[i].id}</td><td style="padding-right:60px">${products[i].name}</td><td style="padding-right:60px">${products[i].brand}</td><td style="padding-right:60px">${products[i].os}</td><td><a href="#" id="x" data-pid=${products[i].id}>X</td></tr></table>`;
-              //products[i].x=0;
-            }
-          }
-          $('#disp').html(result1);
-          
-      }
-
-      displayAll();
-
-      $('#content').on("click","#brand",function(){
-
-        brand=$(this).val();
-        console.log(brand);
-        display();
-
-
-      });
-
-
-      $('#content').on("click","#os",function(){
-
-        os=$(this).val();
-        console.log(os);
-        display();
-      });
-
-      $('#content').on("click","a#x",function(){
-
-        var id=$(this).data("pid");
-        var index="";
-        console.log("hide=="+id);
-        for(let i=0;i<products.length;i++)
-        {
-            if(products[i].id==id)
-            {
-                index=i;
-               
-                //products[i].x="1";
-                //console.log("changed x =="+products[i].x);
-            }
-        }
-        products.splice(index,1);
-        display();
-
-      });
-
-      
-
-
-      function display()
-      {
-          console.log(brand+"=="+os);
-          if(brand=="All"&&os=="All")
-           displayAll();
-         else if(brand=="All")
-           displayByOs();
-         else if(os=="All")
-           displayByBrand();
-        else
-           displayByBoth();  
-      }
-
-
-      function displayByOs()
-      {
-          console.log("display by os");
-          var result2="";
-          if(searchInput=="")
-          {
-            for(let i=0;i<products.length;i++)
-            {
-                if(os==products[i].os )
-                result2+=`<table><tr><td style="padding-right:60px">${products[i].id}</td><td style="padding-right:60px">${products[i].name}</td><td style="padding-right:60px">${products[i].brand}</td><td style="padding-right:60px">${products[i].os}</td><td><a href="#" id="x" data-pid=${products[i].id}>X</td></tr></table>`;
-       
-            }
-          }
-          else
-          {
-            for(let i=0;i<products.length;i++)
-            {
-                if(os==products[i].os && (searchInput==products[i].id || searchInput==products[i].name) )
-                result2+=`<table><tr><td style="padding-right:60px">${products[i].id}</td><td style="padding-right:60px">${products[i].name}</td><td style="padding-right:60px">${products[i].brand}</td><td style="padding-right:60px">${products[i].os}</td><td><a href="#" id="x" data-pid=${products[i].id}>X</td></tr></table>`;
-       
-            }
-
-          }
-          $('#disp').html(result2);
-      }
-
-      function displayByBrand()
-      {
-          console.log("display by brand");
-          var result3="";
-          if(searchInput=="")
-          {
-            for(let i=0;i<products.length;i++)
-            {
-                if(brand==products[i].brand )
-                result3+=`<table><tr><td style="padding-right:60px">${products[i].id}</td><td style="padding-right:60px">${products[i].name}</td><td style="padding-right:60px">${products[i].brand}</td><td style="padding-right:60px">${products[i].os}</td><td><a href="#" id="x" data-pid=${products[i].id}>X</td></tr></table>`;
-       
-            }
-          }
-          else
-          {
-            for(let i=0;i<products.length;i++)
-            {
-                if(brand==products[i].brand && (searchInput==products[i].id || searchInput==products[i].name) )
-                result3+=`<table><tr><td style="padding-right:60px">${products[i].id}</td><td style="padding-right:60px">${products[i].name}</td><td style="padding-right:60px">${products[i].brand}</td><td style="padding-right:60px">${products[i].os}</td><td><a href="#" id="x" data-pid=${products[i].id}>X</td></tr></table>`;
-       
-            }
-
-          }
-          $('#disp').html(result3);
-      }
-
-      function displayByBoth()
-      {
-          console.log("display by both");
-          var result4="";
-          if(searchInput=="")
-          {
-            for(let i=0;i<products.length;i++)
-            {
-                if(os==products[i].os && brand==products[i].brand )
-                result4+=`<table><tr><td style="padding-right:60px">${products[i].id}</td><td style="padding-right:60px">${products[i].name}</td><td style="padding-right:60px">${products[i].brand}</td><td style="padding-right:60px">${products[i].os}</td><td><a href="#" id="x" data-pid=${products[i].id}>X</td></tr></table>`;
-       
-            }
-          }
-          else
-          {
-            for(let i=0;i<products.length;i++)
-            {
-                if(os==products[i].os && brand==products[i].brand && (searchInput==products[i].id || searchInput==products[i].name) )
-                result4+=`<table><tr><td style="padding-right:60px">${products[i].id}</td><td style="padding-right:60px">${products[i].name}</td><td style="padding-right:60px">${products[i].brand}</td><td style="padding-right:60px">${products[i].os}</td><td><a href="#" id="x" data-pid=${products[i].id}>X</td></tr></table>`;
-       
-            }
-          }
-          $('#disp').html(result4);
-      }
-
-
-
-
-
-
-
-
-
-
-      //search 
-
-      $('#content').on("click","#button",function(){
-
-        searchInput=$('#input').val();
-        display();
-        searchInput="";
-        var search="";
-        search+=`<label for="search"><input id="input" placeholder="search by name or id"><input type="button" id="button" value="search"></label>`;
-        $('#search').html(search);
-        console.log(searchInput);
-      });
-
-
+$(document).ready(function()
+{
 
 });
+function add_product()
+{
+    $("#success").css("display","none");
+    $("#error").css("display","none");
+    let table_add ="";
+    let sku = $("#product_sku").val();
+    let name =$("#product_name").val();
+    let price = $("#product_price").val();
+    let quantity = $("#product_quantity").val();  
+    let exp = /^[a-zA-Z\s]+$/;
+    let name_check = exp.test(name);
+
+    // check validation 
+    if(sku =="" || name == "" || price == "" || quantity == "" || isNaN(price) || isNaN(quantity) || !name_check)
+    {
+        $("#error").css("display","block")
+        return;
+    }
+    for(var i=0;i<mall.length;i++)
+    {
+        if(sku == mall[i]["SKU"])
+        {
+            $("#error").css("display","block")
+            return;
+        }
+    }
+    mall.push({"SKU":sku,"NAME":name,"PRICE":price,"QUANTITY":quantity}); //PRODUCT JSON
+    $("#success").css("display","block");
+
+
+    for(var i=0;i<mall.length;i++)
+    {
+       
+        table_add += `<tr id=${mall[i]["SKU"]}><td>${mall[i]["SKU"]}</td><td>${mall[i]["NAME"]}</td><td>${mall[i]["PRICE"]}</td><td>${mall[i]["QUANTITY"]}</td><td><a href="#" class="edit" id=edit${mall[i]["SKU"]} onclick=editform(${mall[i]["SKU"]})>Edit</a><a href="#" class="delete" id=delete${mall[i]["SKU"]} onclick=display_delete_form(${mall[i]["SKU"]})>Delete</a></td</tr>`;
+       
+    } // TO display in HTML
+    $("#display1").html(table_add);
+   
+
+}
+
+var get_udate_index;
+function editform(get_sku)
+{
+
+    $("#add_product").css("display","none");
+    $("#update_product").css("display","block");
+    $("#error").css("display","none");
+    $("#success").css("display","none");
+    for(var i=0;i<mall.length;i++)
+    {
+        if(mall[i]["SKU"] == get_sku)
+        {
+            get_udate_index = i;
+            $("#product_sku").val(mall[i]["SKU"]);
+            $("#product_name").val(mall[i]["NAME"]);
+            $("#product_price").val(mall[i]["PRICE"]);
+            $("#product_quantity").val(mall[i]["QUANTITY"]);
+            break;
+        }
+    }
+    
+    
+}
+function update()
+{
+    $("#add_product").css("display","block");
+    $("#update_product").css("display","none");
+    $("#error").css("display","none");
+  
+    let sku = $("#product_sku").val();
+    let name =$("#product_name").val();
+    let price = $("#product_price").val();
+    let quantity = $("#product_quantity").val();  
+    let exp = /^[a-zA-Z\s]+$/;
+    let name_check = exp.test(name);
+
+    // check validation 
+    if(sku =="" || name == "" || price == "" || quantity == "" || isNaN(price) || isNaN(quantity) || !name_check)
+    {
+        $("#error").css("display","block");
+        return;
+    }
+
+    mall[get_udate_index]["SKU"] = $("#product_sku").val();
+    mall[get_udate_index]["NAME"] = $("#product_name").val();
+    mall[get_udate_index]["PRICE"] =$("#product_price").val();
+    mall[get_udate_index]["QUANTITY"] =$("#product_quantity").val();
+
+    let table_add ="";
+    for(var i=0;i<mall.length;i++)
+    {
+       
+        table_add += `<tr id=${mall[i]["SKU"]}><td>${mall[i]["SKU"]}</td><td>${mall[i]["NAME"]}</td><td>${mall[i]["PRICE"]}</td><td>${mall[i]["QUANTITY"]}</td><td><a href="#" class="edit" id=edit${mall[i]["SKU"]} onclick=editform(${mall[i]["SKU"]})>Edit</a><a href="#" class="delete" id=delete${mall[i]["SKU"]} onclick=display_delete_form(${mall[i]["SKU"]})>Delete</a></td</tr>`;
+       
+    } // TO display in HTML
+    $("#display1").html(table_add);
+    
+}
+
+function display_delete_form(get_sku)
+{
+    $("#add_product_form").css("display","none");
+    $("#yes_no").css("display","block");
+    var disp = `<p>Are you sure want to delete this item</p>
+    <button id="yes" onclick="deleteform(${get_sku})">Yes</button>
+    <button id="no" onclick="remainform()">No</button>`;
+
+    $("#yes_no").html(disp);
+
+
+
+   
+}
+function deleteform(get_sku)
+{
+    for(var i=0;i<mall.length;i++)
+    {
+        if(mall[i]["SKU"] == get_sku)
+        {
+            mall.splice(i,1);
+            break;
+        }
+    }
+    // document.getElementById("add_product_form").style.display = "block";
+    // document.getElementById("yes_no").style.display = "none";
+    $("#add_product_form").css("display","block");
+    $("#yes_no").css("display","none");
+
+
+    let table_add ="";
+    for(var i=0;i<mall.length;i++)
+    {
+       
+        table_add += `<tr id=${mall[i]["SKU"]}><td>${mall[i]["SKU"]}</td><td>${mall[i]["NAME"]}</td><td>${mall[i]["PRICE"]}</td><td>${mall[i]["QUANTITY"]}</td><td><a href="#" class="edit" id=edit${mall[i]["SKU"]} onclick=editform(${mall[i]["SKU"]})>Edit</a><a href="#" class="delete" id=delete${mall[i]["SKU"]} onclick=display_delete_form(${mall[i]["SKU"]})>Delete</a></td</tr>`;
+       
+    } // TO display in HTML
+
+
+    $("#display1").html(table_add)
+    $("#product_sku").val("");
+    $("#product_name").val("");
+    $("#product_price").val("");
+    $("#product_quantity").val("");
+
+
+
+    
+}
+function remainform()
+{
+    $("#add_product_form").css("display","block");
+    $("#yes_no").css("display","none");
+    let table_add ="";
+    for(var i=0;i<mall.length;i++)
+    {
+       
+        table_add += `<tr id=${mall[i]["SKU"]}><td>${mall[i]["SKU"]}</td><td>${mall[i]["NAME"]}</td><td>${mall[i]["PRICE"]}</td><td>${mall[i]["QUANTITY"]}</td><td><a href="#" class="edit" id=edit${mall[i]["SKU"]} onclick=editform(${mall[i]["SKU"]})>Edit</a><a href="#" class="delete" id=delete${mall[i]["SKU"]} onclick=display_delete_form(${mall[i]["SKU"]})>Delete</a></td</tr>`;
+       
+    } // TO display in HTML
+    $("#display1").html(table_add)
+    $("#product_sku").val("");
+    $("#product_name").val("");
+    $("#product_price").val("");
+    $("#product_quantity").val("");
+}
+
+function closesuccess()
+{
+    
+    
+    $("#success").css("display","none");
+}
+function closeerror()
+{
+    $("#error").css("display","none");
+}
